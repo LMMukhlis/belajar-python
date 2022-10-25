e3a173f9fa3b013881d3ec70551dc9dcfb7a627a
LMMukhlis <115677927+LMMukhlis@users.noreply.github.com>
2022-10-25 14:07:01 +0700

    simple practice about "if"

---
+++ nestedif.py
@@ -0,0 +1,13 @@
+a=100
+b=200
+c=300
+
+if a > b:
+    if a > c:
+        print("a value is bigger")
+    else:
+        print("c value is bigger")
+elif b > c:
+    print("b value is bigger")
+else:
+    print("c value is bigger")
---
+++ simpleif.py
@@ -0,0 +1,7 @@
+print("hello world")
+print("Assalamualaikum")
+
+number = int(input("input any number: "))
+
+if number % 2==0:
+    print("that is an even number")
